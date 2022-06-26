13f52582db8e6e41b21f0073ed287e1c356c07eb
ShubhamSehra <sehra.shubham11@gmail.com>
2022-06-26 14:16:02 +0530

    sds

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.delete("/students/delete/:id", async (req, res) => {
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static('frontend/build'));
   app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+    res.sendFile(path.resolve(__dirname,'frontend','frontend/build','frontend/index.html'));
   });
 }
 
