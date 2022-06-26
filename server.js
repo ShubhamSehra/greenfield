4a84b24450f4168f560782ce127db22dd8bd84c1
ShubhamSehra <sehra.shubham11@gmail.com>
2022-06-26 16:10:54 +0530

    dd

--- server.js
+++ server.js
@@ -129,17 +129,14 @@ app.delete("/students/delete/:id", async (req, res) => {
   res.send("/");
 });
 
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-
-app.get("*", (req, res) =>{
-  res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"))
-})
 
 if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('frontend/build'));
-  // app.get('*',(req,res)=>{
-  //   res.sendFile(path.join(__dirname,'frontend','build','index.html'));
-  // });
+  app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+  
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"))
+  })
+
 }
 
 app.listen(PORT, (req, res) => {
