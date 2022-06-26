7a6fa9c22bc9113b1d2be856396c53baa806c9a5
ShubhamSehra <sehra.shubham11@gmail.com>
2022-06-26 13:11:46 +0530

    fixing things

--- frontend/src/components/Navbarr.jsx
+++ frontend/src/components/Navbarr.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Logo from '../logos/school_logo.svg';
 
 function Navbarr()  {
