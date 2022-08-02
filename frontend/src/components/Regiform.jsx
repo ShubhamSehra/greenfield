5922dffa7a577ee470363da69c7938937a028bf5
ShubhamSehra <sehra.shubham11@gmail.com>
2022-08-02 15:04:47 +0530

    form validation

--- frontend/src/components/Regiform.jsx
+++ frontend/src/components/Regiform.jsx
@@ -97,7 +97,7 @@ function Regiform(props) {
       </div>
       <Form
       noValidate validated={validated} 
-      onSubmit={handleSubmit}
+      // onSubmit={handleSubmit}
       >
         <Container>
           <Row>
@@ -240,7 +240,7 @@ function Regiform(props) {
           {!props.id ? (
             <div className="adj-btn">
               <button
-                // onClick={handleSubmit}
+                onClick={handleSubmit}
                 type="submit"
                 className="btn btn-outline-success btn-lg m-3"
               >
